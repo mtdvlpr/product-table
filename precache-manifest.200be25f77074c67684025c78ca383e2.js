959e140dd67eae86c0f87270b3c192030ac533df
Manoah <149895ja@gmail.com>
2020-08-25 12:51:10 +0200

    Updates

---
+++ precache-manifest.200be25f77074c67684025c78ca383e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd9a959b80e9bff2d009f9fe352b512",
+    "url": "/product-table/index.html"
+  },
+  {
+    "revision": "a8650f6445a60bf100a8",
+    "url": "/product-table/static/css/main.603214ed.chunk.css"
+  },
+  {
+    "revision": "2c8b20c6491103cd45c7",
+    "url": "/product-table/static/js/2.a332200a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/product-table/static/js/2.a332200a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8650f6445a60bf100a8",
+    "url": "/product-table/static/js/main.54538880.chunk.js"
+  },
+  {
+    "revision": "3a459df9c2180755f0fc",
+    "url": "/product-table/static/js/runtime-main.061267d4.js"
+  }
+]);
